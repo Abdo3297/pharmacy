173ed0d9454b1cc8868e5041c004eb7d6c173a83
Abdo3297 <abdelrhmanhassanmohamed@gmail.com>
2024-08-01 07:37:02 +0300

    Test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,6 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             UserSeeder::class,
             OrderSeeder::class,
-            ChatSeeder::class,
         ]);
     }
 }
